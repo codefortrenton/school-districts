3e8c8e29640f102ae7e129015a2c8291bb6532f2
james mike dupont <jamesmikedupont@gmail.com>
2016-06-14 07:07:51 -0400

    update

--- convert.sh
+++ convert.sh
@@ -3,5 +3,6 @@ do
     echo $x;
     OUTNAME=`basename $x .shp`
     OUTDIR=`dirname $x`
-    /usr/bin/ogr2ogr -f GeoJSON -t_srs crs:84 ${OUTDIR}/${OUTNAME}.geojson $x
+    #/usr/bin/
+	ogr2ogr -f GeoJSON -t_srs crs:84 ${OUTDIR}/${OUTNAME}.geojson $x
 done
